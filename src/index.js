d699374e1d43ca81a128df737c7cf14c9594e3ec
matlegdal <matlegdal@hotmail.com>
2019-08-04 13:56:34 -0400

    authentification

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 const { GraphQLServer } = require("graphql-yoga")
 const { prisma } = require('./generated/prisma-client')
-
-
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context) => {
-      return context.prisma.links()
-    },
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description
-      })
-    }
-  }
-}
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers,
-  context: { prisma },
+  resolvers: {
+    Query,
+    Mutation,
+    User,
+    Link
+  },
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
