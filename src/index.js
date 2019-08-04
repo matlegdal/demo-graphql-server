1e205e383b064524a6818cd8c834ff0ecb3b1099
matlegdal <matlegdal@hotmail.com>
2019-08-04 16:53:58 -0400

    finish tutorial with subscription and filtering

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
+    Vote,
     User,
     Link
   },
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,23 @@
-function feed(parent, args, context, info) {
-  return context.prisma.links()
+async function feed(parent, args, context, info) {
+  const where = args.filter ? {description_contains: args.filter} : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  });
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count()
+
+  return {
+    links,
+    count,
+  }
 }
 
 module.exports = {
